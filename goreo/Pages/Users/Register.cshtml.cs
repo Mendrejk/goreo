eb4ee2439dd2f981fa71f356f1281c7dd3718743
Mendrejk <bastian.lakomy@gmail.com>
2022-01-22 23:34:59 +0100

    add profile image handling during registration

--- goreo/Pages/Users/Register.cshtml.cs
+++ goreo/Pages/Users/Register.cshtml.cs
@@ -1,21 +1,23 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using goreo;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace goreo.Pages.Users
 {
     public class RegisterModel : PageModel
     {
         private readonly goreo.postgresContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public RegisterModel(goreo.postgresContext context)
+        public RegisterModel(goreo.postgresContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult OnGet()
@@ -24,8 +26,7 @@ namespace goreo.Pages.Users
             return Page();
         }
 
-        [BindProperty]
-        public User Registrant { get; set; }
+        [BindProperty] public Registrant Registrant { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
@@ -35,10 +36,34 @@ namespace goreo.Pages.Users
                 return Page();
             }
 
-            _context.Users.Add(Registrant);
+            // Save image to wwwroot/uploads
+            var wwwRootPath = _hostEnvironment.WebRootPath;
+
+            if (Registrant.ImageFile != null) // if null, then the default image shall be used
+            {
+                var fileName = Path.GetFileNameWithoutExtension(Registrant.ImageFile.FileName);
+                var extension = Path.GetExtension(Registrant.ImageFile.FileName);
+                
+                fileName += DateTime.Now.ToString("yymmssfff") + extension;
+                Registrant.User.ProfileImage = fileName;
+                
+                var path = Path.Combine(wwwRootPath + "/profilePictures", fileName);
+
+                await using var fileStream = new FileStream(path, FileMode.Create);
+                await Registrant.ImageFile.CopyToAsync(fileStream);
+            }
+
+            // Insert the new user into the db
+            _context.Users.Add(Registrant.User);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
+    }
+
+    public class Registrant
+    {
+        public User User { get; set; }
+        public IFormFile ImageFile { get; set; }
     }
 }
