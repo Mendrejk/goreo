56c78e2bcf3c42b9bd05e9d50d297b2d7c31a19c
Mendrejk <bastian.lakomy@gmail.com>
2022-01-23 01:15:07 +0100

    fix Users/Index not working if a profile picture is not present in wwwroot

--- goreo/Pages/Users/Index.cshtml.cs
+++ goreo/Pages/Users/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using goreo;
 using Microsoft.AspNetCore.Authorization;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace goreo.Pages.Users
 {
@@ -14,18 +16,33 @@ namespace goreo.Pages.Users
     public class IndexModel : PageModel
     {
         private readonly goreo.postgresContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public IndexModel(goreo.postgresContext context)
+        public IndexModel(goreo.postgresContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostEnvironment = hostEnvironment;
         }
 
-        public IList<User> User { get;set; }
+        public IList<UserWithImagePath> UsersWithImagePath { get; set; }
 
         public async Task OnGetAsync()
         {
-            User = await _context.Users
+            var users = await _context.Users
                 .Include(u => u.Booklet).ToListAsync();
+
+            UsersWithImagePath = users.Select(user =>
+                {
+                    var imagePath = user.ProfileImage == null
+                                    || !System.IO.File.Exists(Path.Combine(
+                                        _hostEnvironment.WebRootPath + "/profilePictures",
+                                        user.ProfileImage))
+                        ? "~/photos/DefaultProfileImage.png"
+                        : "~/profilePictures/" + user.ProfileImage;
+
+                    return new UserWithImagePath { User = user, ImagePath = imagePath };
+                }
+            ).ToList();
         }
     }
 }
---
+++ goreo/Pages/Users/UserWithImagePath.cs
@@ -0,0 +1,8 @@
+﻿namespace goreo.Pages.Users
+{
+    public class UserWithImagePath
+    {
+        public User User { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
