ad7c1273ff06389b3c3dc8c1228bf1c6a82661ee
Mendrejk <bastian.lakomy@gmail.com>
2022-01-23 12:47:11 +0100

    add user's routes fetching logic to the Routes/Index page

--- goreo/Pages/Routes/Index.cshtml.cs
+++ goreo/Pages/Routes/Index.cshtml.cs
@@ -1,13 +1,46 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace goreo.Pages.Routes
 {
     [Authorize(Policy = "MustBeUser")]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly goreo.postgresContext _context;
+
+        public IndexModel(postgresContext context)
         {
+            _context = context;
+        }
+
+        public IList<Route> Routes { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var userClaims = ((ClaimsIdentity)User.Identity)?.Claims;
+            if (userClaims == null)
+            {
+                return RedirectToPage("/Users/Logout");
+            }
+
+            var username = userClaims.Where(claim =>
+                claim.Type == ClaimTypes.Name).Select(claim => claim.Value).SingleOrDefault();
+
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.Username == username);
+            if (user == null)
+            {
+                return RedirectToPage("/Users/Logout");
+            }
+
+            Routes = user.Routes.ToList();
+
+            return Page();
         }
     }
 }
