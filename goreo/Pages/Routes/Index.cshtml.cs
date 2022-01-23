d0928d858b99d5fee0daf8a937ae6875b6a2f1d9
Mendrejk <bastian.lakomy@gmail.com>
2022-01-23 01:50:50 +0100

    start work on Routes pages

---
+++ goreo/Pages/Routes/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace goreo.Pages.Routes
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
