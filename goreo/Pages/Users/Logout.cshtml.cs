2b81011009399069611c36149794c3456780945c
Mendrejk <bastian.lakomy@gmail.com>
2022-01-23 02:11:26 +0100

    add logout page

---
+++ goreo/Pages/Users/Logout.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace goreo.Pages.Users
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            
+            return RedirectToPage("/Users/Login");
+        }
+    }
+}
