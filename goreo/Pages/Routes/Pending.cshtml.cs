9212345925d1b71f143731f540e345ffeb2399a9
Mendrejk <bastian.lakomy@gmail.com>
2022-01-25 11:03:25 +0100

    don't list the leader's routes in Routes/Pending

--- goreo/Pages/Routes/Pending.cshtml.cs
+++ goreo/Pages/Routes/Pending.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -22,13 +23,25 @@ namespace goreo.Pages.Routes
 
         public IList<BookletsRoute> BookletsRoutes { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
+            var userClaims = ((ClaimsIdentity)User.Identity)?.Claims;
+            if (userClaims == null)
+            {
+                return RedirectToPage("/Users/Logout");
+            }
+
+            var username = userClaims.Where(claim =>
+                claim.Type == ClaimTypes.Name).Select(claim => claim.Value).SingleOrDefault();
+
             BookletsRoutes = await _context.BookletsRoutes
                 .Where(bookletsRoute => !bookletsRoute.isConfirmed)
                 .Include(b => b.Booklet)
                 .ThenInclude(booklet => booklet.User)
+                .Where(bookletsRoute => bookletsRoute.Booklet.User.Username != username)
                 .Include(b => b.Route).ToListAsync();
+
+            return Page();
         }
     }
 }
